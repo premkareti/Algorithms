7667c8e0b5099d616d20924c18fde5c70154cb75
premkareti <premkareti@gmail.com>
2016-01-04 16:28:38 -0800

    Add Insertion Sort

---
+++ AlgosStudy/InsertionSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgosStudy
+{
+  public class InsertionSort
+  {
+    public int[] Sort(int[] inputArr, bool isAscending = true)
+    {
+      int length = inputArr.Length;
+      int[] retVal = new int[length];
+      for (int i = 1; i < length; i++)
+      {
+        int j = i;
+        while (j > 0 && inputArr[j] < inputArr[j - 1])
+        {
+          int k = inputArr[j];
+          inputArr[j] = inputArr[j - 1];
+          inputArr[j - 1] = k;
+          j--;
+        }
+      }
+      inputArr.CopyTo(retVal, 0);
+      if (!isAscending)
+        return retVal.Reverse().ToArray();
+
+      return retVal;
+    }
+  }
+}
--- AlgosStudy/Program.cs
+++ AlgosStudy/Program.cs
@@ -10,6 +10,22 @@ namespace AlgosStudy
   {
     static void Main(string[] args)
     {
+      int[] inputVals = new int[] { 7, 3, -1, 5, 6, 4, 0 };
+      Console.WriteLine("Input Array: " + PrintArray(inputVals));
+      InsertionSort insSort = new InsertionSort();
+      Console.WriteLine("After Insertion Sort: " + PrintArray(insSort.Sort(inputVals)));
+      Console.WriteLine("Press any key to exit");
+      Console.ReadKey();
+    }
+
+    private static string PrintArray(int[] inVals)
+    {
+      StringBuilder temp = new StringBuilder();
+      foreach(var val in inVals)
+      {
+        temp.Append(val + " ");
+      }
+      return temp.ToString();
     }
   }
 }
